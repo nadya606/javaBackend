3305c8e3f098b62102cdd929aa0de516259faaf1
nadya <n.ivanova606@gmail.com>
2021-11-14 11:46:17 +0300

    jb_dz4

--- src/main/java/jb_dz3/AccountTests.java
+++ src/main/java/jb_dz3/AccountTests.java
@@ -15,6 +15,23 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class AccountTests extends BaseTest{
     @Test
+    void getAccountInfoTest2() {
+        given(requestSpecificationWithAuth)
+                .when()
+                .get("https://api.imgur.com/3/account/{username}",username)
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    void getAccountInfoTest3() {
+        given(requestSpecificationWithAuth, positiveResponseSpecification)
+                .get("https://api.imgur.com/3/account/{username}",username)
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
     void getAccountInfoTest() {
         given()
                 .header("Authorization", token)
@@ -23,6 +40,8 @@ public class AccountTests extends BaseTest{
                 .then()
                 .statusCode(200);
     }
+
+
 
     @Test
     void getAccountInfoWithLoggingTest() {
--- src/main/java/jb_dz3/BaseTest.java
+++ src/main/java/jb_dz3/BaseTest.java
@@ -2,19 +2,29 @@ package jb_dz3;
 
 import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.filter.Filter;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.BeforeAll;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
+
+import java.io.*;
 import java.util.List;
 import java.util.Properties;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
 public abstract class BaseTest {
+    private static final String PATH_TO_IMAGE = "/image";
     static Properties properties = new Properties();
     static String token;
     static String username;
+    static ResponseSpecification positiveResponseSpecification;
+    static RequestSpecification requestSpecificationWithAuth;
 
     @BeforeAll
     static void beforeAll(){
@@ -23,6 +33,21 @@ public abstract class BaseTest {
         getProperties();
         token = properties.getProperty("token");
         username = properties.getProperty("username");
+
+        requestSpecificationWithAuth = new RequestSpecBuilder()
+                .addHeader("Authorization", token)
+                .build();
+
+        positiveResponseSpecification = new ResponseSpecBuilder()
+                .expectBody("status", equalTo(200))
+                .expectBody("success", is(true))
+//                .expectResponseTime(lessThanOrEqualTo(500L))
+                .expectContentType(ContentType.JSON)
+                .expectStatusCode(200)
+                .build();
+
+
+
     }
 
     private static void getProperties() {
@@ -32,6 +57,16 @@ public abstract class BaseTest {
         catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public byte[] getFileContent(String path) {
+        byte [] byteArray = new byte[0];
+        try {
+            byteArray = FileUtils.readFileToByteArray(new File(PATH_TO_IMAGE));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return byteArray;
     }
 
 }
